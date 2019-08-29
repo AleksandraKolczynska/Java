ceda920d9897ad21c7fe0aa94d0ecbbf5273c943
AleksandraKolczynska <54152507+AleksandraKolczynska@users.noreply.github.com>
2019-08-29 12:21:52 +0200

    Update and rename main.java to Main.java

--- example of implements and extends/Main.java
+++ example of implements and extends/Main.java
@@ -1,13 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class main {
+public class Main {
 
 	public static void main(String[] args) {
 		List<organism> list = new ArrayList();
-		list.add(new dog(10, "fetching", "male","Morus"));
-		list.add(new dog(8, "digging", "male","Kubus"));
-		list.add(new cat(14, "mouse", "male", "Julek"));
+		list.add(new Dog(10, "fetching", "male","Morus"));
+		list.add(new Dog(8, "digging", "male","Kubus"));
+		list.add(new Cat(14, "mouse", "male", "Julek"));
 		list.stream().forEach(System.out::println);
 		
 		
