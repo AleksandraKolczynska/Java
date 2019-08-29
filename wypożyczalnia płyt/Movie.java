70092d36e78231b7b08ec47783205c96630ac348
AleksandraKolczynska <54152507+AleksandraKolczynska@users.noreply.github.com>
2019-08-29 12:53:30 +0200

    Update and rename movie.java to Movie.java

--- wypożyczalnia płyt/Movie.java
+++ wypożyczalnia płyt/Movie.java
@@ -1,37 +1,42 @@
 
-public class movie {
+public class Movie {
 	String title;
 	String director;
 	double price;
-	public movie (String title, String director, double price) {
+
+	public Movie(String title, String director, double price) {
 		this.director = director;
 		this.title = title;
-		this.price = price;	
+		this.price = price;
 	}
-	public void settitle (String title) {
+
+	public void settitle(String title) {
 		this.title = title;
 	}
+
 	public String gettitle() {
 		return title;
 	}
+
 	public String getdirector() {
 		return director;
 	}
+
 	public void setdirector(String director) {
 		this.director = director;
 	}
+
 	public double getPrice() {
 		return price;
 	}
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
-	
-	public String toString () {
-		return "Title: " +title +" Director: " + director +" Price: " + price + "z³";
-		
+
+	public String toString() {
+		return "Title: " + title + " Director: " + director + " Price: " + price + "zÂ³";
+
 	}
-	
-	
 
 }
